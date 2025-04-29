152a898eefb791a0f87403e2f0a18c2e48e294e6
Jongbok <105177081+jongbokhi@users.noreply.github.com>
2025-04-29 23:54:30 +0200

    Add files via upload

---
+++ Self-RAG/README.md
@@ -0,0 +1,44 @@
+# 📄 Self-RAG
+
+## Overview
+
+This project implements a **Self-RAG pipeline**.  
+
+**[Reference: Self-RAG](https://arxiv.org/abs/2310.11511)**
+
+Self-RAG is a RAG approach that involves self-reflection and self-evaluation based on the retrieved documents and the generated answer.
+
+Unlike traditional RAG, this pipeline introduces **Document Grading**, **Query Transformation**, **Relevance Check** and **Hallucination Check**  steps to generate **more accurate and reliable answers**.
+
+The pipeline uses the **Samsung Electronics 2024 Sustainability Report (Korean version)** as context.
+
+---
+
+## Key Features
+
+- **Document Grading:**  
+  a grader assessing relevance of a retrieved document to a user question..
+
+- **Query Transformation:**  
+  a question re-writer that converts an input question to a better version that is optimized for vectorstore retrieval
+  
+- **Hallucination Check:**  
+  a grader assessing whether an LLM generation is grounded in supported by a set of retrieved facts.
+
+- **Relevance Check:**  
+  a grader assessing whether an answer addresses and resolves a question
+
+- **Iterative Improvement:**  
+  Through document grading and query refinement, the model continuously improves the answer quality.
+
+---
+
+## Pipeline Structure
+![RAG_pipeline](https://github.com/user-attachments/assets/e4381679-b111-4329-8be5-97d939180a85)
+
+---
+
+## Context
+
+- 📚 **Samsung Electronics Sustainability Report 2024 (Korean)**
+- The data is indexed into an internal vector store (FAISS) for retrieval.
---
+++ Self-RAG/adaptiverag/retrievalgrader.py
@@ -0,0 +1,38 @@
+
+from langchain_core.prompts import ChatPromptTemplate
+from pydantic import BaseModel, Field
+from langchain_openai import ChatOpenAI
+
+
+class GradeDocuments(BaseModel):
+    """Binary score for relevance check on retrieved documents."""
+
+    binary_score: str = Field(
+        description="Documents are relevant to the question, 'yes' or 'no'"
+    )
+
+
+def retrievalgrader(model_name):
+    # LLM initialization and structured output generation via function calling
+    llm = ChatOpenAI(model=model_name, temperature=0)
+    structured_llm_grader = llm.with_structured_output(GradeDocuments)
+
+    # Creating a prompt template including system messages and user questions
+    system = """You are a grader assessing relevance of a retrieved document to a user question. \n 
+        If the document contains keyword(s) or semantic meaning related to the user question, grade it as relevant. \n
+        It does not need to be a stringent test. The goal is to filter out erroneous retrievals. \n
+        Give a binary score 'yes' or 'no' score to indicate whether the document is relevant to the question."""
+
+    grade_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system),
+            (
+                "human",
+                "Retrieved document: \n\n {document} \n\n User question: {question}",
+            ),
+        ]
+    )
+    # Building a Retrieval Grader by integrating a prompt template with a structured LLM-based router
+    retrieval_grader = grade_prompt | structured_llm_grader
+
+    return retrieval_grader
